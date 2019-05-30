9856b366849956b5c43b9756a4ba2d251e13cc78
horihiro <hori.kawa.shima.tool+github@gmail.com>
2019-05-30 21:04:12 +0900

    change example

--- example/app.ts
+++ example/app.ts
@@ -1,10 +1,10 @@
 import { serve } from "https://deno.land/std/http/server.ts";
-const PORT = Deno.env().PORT || 3000
+const PORT = Deno.env().PORT || 80;
 const s = serve(`0.0.0.0:${PORT}`);
 
 async function main() {
   for await (const req of s) {
-    req.respond({ body: new TextEncoder().encode("Hello World\n") });
+    req.respond({ body: new TextEncoder().encode("Hello Deno\n") });
   }
 }
 
